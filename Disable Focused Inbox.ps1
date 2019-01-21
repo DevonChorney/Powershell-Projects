52e050e3eeb39316484c67c396dbfeb429be3d37
reaper1001 <devon.chorney@gmail.com>
2019-01-21 16:31:54 -0700

    Add files via upload

---
+++ Disable Focused Inbox.ps1
@@ -0,0 +1,6 @@
+﻿$CRED = Get-Credential
+$SESSION = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $CRED -Authentication Basic -AllowRedirection 
+Import-PSSession $SESSION -AllowClobber
+Connect-MsolService -Credential $CRED
+Set-OrganizationConfig -FocusedInboxOn $False
+Write-Host Focused Inbox is now disabled
