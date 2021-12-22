918716c5af932ef333a7c42c7c3109492b6cdc25
PigeonNoises <25204018+DevonChorney@users.noreply.github.com>
2021-12-22 07:57:11 -0700

    Add files via upload
    Manually created a CSV from the primary client numbering scheme.

---
+++ ncentral get client users.ps1
@@ -0,0 +1,7 @@
+﻿$CustomerList = Get-Content C:\temp\customer.csv
+foreach ($Customer in $CustomerList){
+Get-NCCustomerList $Customer | Select-object parentcustomername -ExpandProperty parentcustomername | Out-file C:\temp\ncentral.csv -Append
+Write-Host "Scanning $Customer for active users"
+$Customer | Out-file C:\temp\ncentral.csv -Append
+Get-NCUserRoleList -CustomerID $Customer | select-object -Property rolename, usernames | fl | Out-File c:\temp\ncentral.csv -Append
+}
