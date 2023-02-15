6e1340fd7d3fa6a0b03b9bec6a2b2a0c63bf9dd2
PigeonNoises <25204018+DevonChorney@users.noreply.github.com>
2023-02-15 15:09:26 -0700

    Update ncentral get client users.ps1

--- ncentral get client users.ps1
+++ ncentral get client users.ps1
@@ -1,13 +1,15 @@
 $CustomerLookUp = Get-NCCustomerList | where-object { $_.parentid -eq '50' } | Select-Object customerid, parentid, customername
 $NCRoles = foreach ($Customer in $CustomerLookUp) {
     $Roles = Get-NCUserRolelist -CustomerIDs $Customer.customerid | Where-Object { $_.usernames -ne "[]" } 
-    if ($null -eq $roles){
-    [PSCustomObject]@{
-        rolename     = $_.rolename
-        customerid   = $_.customerid
-        customername = $customer.customername
-        usernames    = $_.usernames
+    if ($roles) {
+        foreach ($role in $roles) {
+            [PSCustomObject]@{
+                rolename     = $role.rolename
+                customerid   = $role.customerid
+                customername = $customer.customername
+                usernames    = $role.usernames
+            }
+        }
     }
-}
 }
 $NCRoles | Export-Csv "c:\temp\ncentral.csv" -NoTypeInformation -Encoding UTF8
