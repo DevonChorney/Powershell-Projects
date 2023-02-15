42728566e60d86e94685a11fc93df2e5a8badd7d
PigeonNoises <25204018+DevonChorney@users.noreply.github.com>
2023-02-15 14:58:51 -0700

    Update ncentral get client users.ps1

--- ncentral get client users.ps1
+++ ncentral get client users.ps1
@@ -1,27 +1,13 @@
-$CustomerList = Get-NCCustomerList | Select-Object customername,parentid,customerid
-
-foreach ($Customer in $CustomerList){
-    if($Customer.parentid -eq 50){
-    $TableOutput = @{}
-    Get-NCCustomerList $Customer.customerid | Select-object parentcustomername -ExpandProperty parentcustomername -Unique
-    $Customername = $Customer.customername
-    Write-Host "Scanning '$Customername' for active users"
-    "
-    $Customername
-    "  | Out-file C:\temp\ncentraluseraudit.csv -Append
-    $UserRoles = Get-NCUserRoleList -CustomerID $Customer.customerid | select-object -Property rolename, usernames
-    foreach ($UserRole in $UserRoles){
-        if($UserRole.usernames -ne '[]'){
-            Write-Host $UserRole.rolename
-            Write-Host $UserRole.usernames
-            $Tableoutput.Add($UserRole.rolename,$UserRole.usernames)
-    
-            
-            }else{
-            Write-Host "Empty Role"
-            
-            }}
-$Tableoutput | Out-file C:\temp\ncentraluseraudit.csv -Append
-}else{
-Write-Host "Not a primary site"
-}}
+$CustomerLookUp = Get-NCCustomerList | where-object { $_.parentid -eq '50' } | Select-Object customerid, parentid, customername
+$NCRoles = foreach ($Customer in $CustomerLookUp) {
+    $Roles = Get-NCUserRolelist -CustomerIDs $Customer.customerid | Where-Object { $_.usernames -ne "[]" } 
+    if ($null -eq $roles){
+    [PSCustomObject]@{
+        rolename     = $_.rolename
+        customerid   = $_.customerid
+        customername = $customer.customername
+        usernames    = $_.usernames
+    }
+}
+}
+$NCRoles | Export-Csv "c:\temp\ncentral.csv" -NoTypeInformation -Encoding UTF8
